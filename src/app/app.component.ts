c34fd8a9eeb7d1266bc56eb9b86636c1afc3ccec
cdoblas <cdoblas.eng@gmail.com>
2024-04-16 07:54:59 +0200

    feature: add top bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css', 'top-bar/top-bar.component.css'],
 })
 export class AppComponent {
   reserv = true;
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-top-bar',
+  selector: 'top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
