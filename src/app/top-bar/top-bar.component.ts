5180240e5fd07d00f68169030fbd200da56d1f3c
Carlos Doblas <cdoblas.eng@gmail.com>
2024-05-23 21:02:05 +0200

    feature: new top-bar-component

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent {
 
+export class TopBarComponent {
 }
 
 
